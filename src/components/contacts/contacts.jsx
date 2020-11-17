4680fe5e393af578533e2a995c8f03319003ae4f
Eyhenij <Eyhenij@gmail.com>
2020-11-17 10:09:52 +0300

    17.11.2020-001 add class Contacts

--- src/components/contacts/contacts.jsx
+++ src/components/contacts/contacts.jsx
@@ -1,27 +1,70 @@
 import React from 'react';
 import s from './contacts.module.css';
 import * as axios from 'axios';
-import avatar from './../../assets/images/avatar.webp'
+import avatar from '../../assets/images/avatar.webp'
 
-const Contacts = (props) => {
+// const Contacts = (props) => {
+// let getUsers = () => {
+//     if (props.arrUsers.length === 0) {
+//         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+//             props.setUsers(response.data.items)
+//         })
+//     }
+// }
+//
+//     return <div className={s.contacts__page}>
+//         <button onClick={getUsers}>Get Users</button>
+//         {
+//             props.arrUsers.map(c => <div key={c.id} className={s.community__item}>
+//                 <span>
+//                     <div>
+//                         <img className={s.avatarCapture} src={c.photos.small != null ? c.photos.small: avatar} alt={'аватарка'} />
+//                     </div>
+//                     <div>
+//                         { c.followed
+//                             ? <button onClick = { () => { props.unFollow(c.id) } }>Unfollow</button>
+//                             : <button onClick = { () => { props.follow(c.id) } }>Follow</button>
+//                         }
+//                     </div>
+//                 </span>
+//                 <span className={s.item__description}>
+//                     <div className={s.item__name}>
+//                         {c.name}
+//                     </div>
+//                     <div>
+//                         {c.status}
+//                     </div>
+//                 </span>
+//                 </div>
+//             )
+//         }
+//     </div>
+// }
 
-    if (props.arrUsers.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then( response => {
-            props.setUsers(response.data.items)
+
+class Contacts extends React.Component {
+
+    getUsers = () => {
+    if (this.props.arrUsers.length === 0) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.props.setUsers(response.data.items)
         })
     }
+}
 
-    return <div className={s.contacts__page}>
-        {
-            props.arrUsers.map(c => <div key={c.id} className={s.community__item}>
+    render() {
+        return <div className={s.contacts__page}>
+            <button onClick={this.getUsers}>Get Users</button>
+            {
+            this.props.arrUsers.map(c => <div key={c.id} className={s.community__item}>
                 <span>
                     <div>
                         <img className={s.avatarCapture} src={c.photos.small != null ? c.photos.small: avatar} alt={'аватарка'} />
                     </div>
                     <div>
                         { c.followed
-                            ? <button onClick = { () => { props.unFollow(c.id) } }>Unfollow</button>
-                            : <button onClick = { () => { props.follow(c.id) } }>Follow</button>
+                            ? <button onClick = { () => { this.props.unFollow(c.id) } }>Unfollow</button>
+                            : <button onClick = { () => { this.props.follow(c.id) } }>Follow</button>
                         }
                     </div>
                 </span>
@@ -37,7 +80,7 @@ const Contacts = (props) => {
             )
         }
     </div>
+    }
 }
-
 
 export default Contacts;
